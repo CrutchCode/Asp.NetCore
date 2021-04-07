ec2060b6e5fd917cc75b8263af86498ccc80177c
Serik <Serik@DESKTOP-7LVQVVL>
2021-04-07 22:52:57 +0300

    Добавьте файлы проекта.

---
+++ 3DPlastSiteMVC/Models/PlastContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3DPlastSiteMVC.Models
+{  
+    public class PlastContext : DbContext
+    {
+        public PlastContext(DbContextOptions<PlastContext>options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ 3DPlastSiteMVC/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _3DPlastSiteMVC.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string MiddleName { get; set; }
+        public string UserEmail { get; set; }
+        public string UserPhone { get; set; }
+    }
+}

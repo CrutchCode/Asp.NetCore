d3ff35e78493a785a62de66967c8837dfb51ae5a
Serik <Serik@DESKTOP-7LVQVVL>
2021-04-08 18:05:08 +0300

    Add custom button

---
+++ 3DPlastSiteMVC/Controllers/ServicesController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PlastSiteMVC.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult GallaryPhotosOfProducts()
+        {
+            return View();
+        }
+        public IActionResult PhotoPrinter()
+        {
+            return View();
+        }
+        public IActionResult CustomModelDevelopment()
+        {
+            return View();
+        }
+        public IActionResult CustomPrinting()
+        {
+            return View();
+        }
+    }
+}
